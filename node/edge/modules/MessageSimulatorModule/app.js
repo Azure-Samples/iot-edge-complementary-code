d382e0e0f2fe0a960ca3ef3c804f5771c67ba013
Alan Pulliam <apulliam@microsoft.com>
2019-07-10 12:42:48 -0400

    Initial checkin with MessageSimulatorModule

--- node/edge/modules/MessageSimulatorModule/app.js
+++ node/edge/modules/MessageSimulatorModule/app.js
@@ -4,8 +4,6 @@ var Transport = require('azure-iot-device-mqtt').Mqtt;
 var Client = require('azure-iot-device').ModuleClient;
 var Message = require('azure-iot-device').Message;
 
-var temperatureThreshold = 21.5;
-
 Client.fromEnvironment(Transport, function (err, client) {
   if (err) {
     throw err;
@@ -21,26 +19,28 @@ Client.fromEnvironment(Transport, function (err, client) {
       } else {
         console.log('IoT Hub module client initialized');
 
+        while (true)
+        {
+          
+        }
+
         // Act on input messages to the module.
         client.on('inputMessage', function (inputName, msg) {
-          filterMessage(client, inputName, msg);
+          pipeMessage(client, inputName, msg);
         });
       }
     });
   }
 });
 
-// This function filters out messages that report temperatures below the temperature threshold.
-// It also adds the MessageType property to the message with the value set to Alert.
-function filterMessage(client, inputName, msg) {
+// This function just pipes the messages without any change.
+function pipeMessage(client, inputName, msg) {
   client.complete(msg, printResultFor('Receiving message'));
+
   if (inputName === 'input1') {
     var message = msg.getBytes().toString('utf8');
-    var messageBody = JSON.parse(message);
-    if (messageBody && messageBody.machine && messageBody.machine.temperature && messageBody.machine.temperature > temperatureThreshold) {
-      console.log(`Machine temperature ${messageBody.machine.temperature} exceeds threshold ${temperatureThreshold}`);
+    if (message) {
       var outputMsg = new Message(message);
-      outputMsg.properties.add('MessageType', 'Alert');
       client.sendOutputEvent('output1', outputMsg, printResultFor('Sending received message'));
     }
   }
