a2c080c9400c8b4aac2ab76b98b780188ce6496c
Alan Pulliam <apulliam@microsoft.com>
2019-07-10 23:57:45 -0400

    node simulator working, but can't debug

--- node/edge/modules/MessageSimulatorModule/app.js
+++ node/edge/modules/MessageSimulatorModule/app.js
@@ -3,80 +3,45 @@
 var Transport = require('azure-iot-device-mqtt').Mqtt;
 var Client = require('azure-iot-device').ModuleClient;
 var Message = require('azure-iot-device').Message;
-var Fs = require('fs');
-var Path = require('path');
+const fs = require('fs');
+const glob = require('glob');
+const util = require('util');
 
+const readFileAsync = util.promisify(fs.readFile);
+const globAsync = util.promisify(glob);
+const fromEnvironmentAsync = util.promisify(Client.fromEnvironment);
 
 async function run() {
 
   let interval = 30000;
-  let folder = "messages";
+  let folder = "messages/";
   let prefix = "*.xml";
   let outputChannelName = "output"
 
-  Client.fromEnvironment(Transport, (err, client) => {
-    if (err) {
-      throw err;
-    } else {
-      client.on('error', function (err) {
-        throw err;
-      });
+  let client = await fromEnvironmentAsync(Transport);
+ 
+  await client.open();
+  console.log('client initialized');
+ 
+  let path = folder + prefix;
 
-      // connect to the Edge instance
-      client.open(function (err) {
-        if (err) {
-          throw err;
-        } else {
-          console.log('IoT Hub module client initialized');
+  const done = false;
 
-          let path = folder + prefix;
+  while (!done) {
+    let files = await globAsync(path);
 
-          const done = false;
+    console.log(files.length);
+    for (let i = 0; i < files.length; i++) {
 
-          while (!done) {
-            let files = await globAsync(path);
-            for (let i = 0; i < files.length; i++) {
-
-              let data = await readFileAsync(files[i], {});
-              let response = await client.sendOutputEvent(outputChannelName, new Message(data));
-              await sleep(interval);
-            }
-          }
-
-        }
-      });
-    }
-  });
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
-// This function just pipes the messages without any change.
-function pipeMessage(client, inputName, msg) {
-  client.complete(msg, printResultFor('Receiving message'));
-
-  if (inputName === 'input1') {
-    var message = msg.getBytes().toString('utf8');
-    if (message) {
-      var outputMsg = new Message(message);
-      client.sendOutputEvent('output1', outputMsg, printResultFor('Sending received message'));
+      let data = await readFileAsync(files[i], {});
+      let response = await client.sendOutputEvent(outputChannelName, new Message(data));
+      await sleep(interval);
     }
   }
 }
 
-// Helper function to print results in the console
-function printResultFor(op) {
-  return function printResult(err, res) {
-    if (err) {
-      console.log(op + ' error: ' + err.toString());
-    }
-    if (res) {
-      console.log(op + ' status: ' + res.constructor.name);
-    }
-  };
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 run();
