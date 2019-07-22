02712660b51884a8115399373c1e98d84fd74481
Alan Pulliam <apulliam@microsoft.com>
2019-07-22 09:32:34 -0700

    Change unit tests to use real data

--- csharp/shared/CompressionTests/CompressionTests.cs
+++ csharp/shared/CompressionTests/CompressionTests.cs
@@ -1,45 +1,44 @@
 ﻿using Xunit;
 using DataCompression;
-// using Microsoft.Azure.Devices.Client;
+using System.IO;
 
 namespace CompressionTests
 {
-    public class CompressionTests
+    public class CompressionTests 
     {
-        // private static Message emptyTestMsg = new Message();
         private static byte[] emptyMsgData = new byte[]{};
-
-        //decompressedContentData is the string "this is a message." in decimal.
-        private static byte[] decompressedContentData = new byte[] {116, 104, 105, 115, 32, 105, 115, 32, 
-            97, 32, 109, 101, 115, 115, 97, 103, 101, 46};
-
-        //compressedContentData is the decompressedContentData result using the gzip library.
-        private static byte[] compressedContentData = new byte[] {31, 139, 8, 0, 0, 0, 0, 0, 0, 11, 43, 201, 200, 44,
-            86, 0, 162, 68, 133, 220, 212, 226, 226, 196, 244, 84, 61, 0, 249, 210, 81, 69, 18, 0, 0, 0};
-
-        // private static Message decompressedMsg = new Message(decompressedContentData);
-        // private static Message compressedMsg = new Message(compressedContentData);
-
+        private byte[] decompressedContentData;
+        private byte[] compressedContentData;
+     
+        public CompressionTests()
+        {
+            decompressedContentData = File.ReadAllBytes("test/1.xml");
+            compressedContentData = File.ReadAllBytes("test/1.xml.gz");
+        }
 
         [Fact]
-        public void Compression_Function_Empty_Message_Data_Returns_Empty_Message_Data(){
-            Assert.Equal(emptyMsgData,DataCompression.CompressionClass.Compress(emptyMsgData));
+        public void Compress_Empty_Buffer_Returns_Empty_Buffer()
+        {
+            Assert.Equal(emptyMsgData, CompressionClass.Compress(emptyMsgData));
         }
         
         [Fact]
-        public void Compression_Function_Message_Data_Is_Returned_With_Compressed_Data(){
-            Assert.Equal(compressedContentData, DataCompression.CompressionClass.Compress(decompressedContentData));
+        public void Compress_Decompress_Returns_Original_Data()
+        {
+            var compressedData = CompressionClass.Compress(decompressedContentData);
+            Assert.Equal(decompressedContentData, CompressionClass.Decompress(compressedData));
         }
 
         [Fact]
-        public void Decompression_Function_Empty_Message_Data_Returns_Empty_Message_Data(){
-            Assert.Equal(emptyMsgData,DataCompression.CompressionClass.Decompress(emptyMsgData));
+        public void Decompress_Empty_Data_Returns_Empty_Data()
+        {
+            Assert.Equal(emptyMsgData, CompressionClass.Decompress(emptyMsgData));
         }
 
         [Fact]
-        public void Decompression_Function_Compressed_Message_Data_Is_Returned_As_Decompressed_Message_Data(){
-            var contents = DataCompression.CompressionClass.Compress(decompressedContentData);
-            Assert.Equal(decompressedContentData,DataCompression.CompressionClass.Decompress(contents));
+        public void Decompress_External_GZip_Data_Returns_Original_Data()
+        {
+            Assert.Equal(decompressedContentData, CompressionClass.Decompress(compressedContentData));
         }
     }
 }
