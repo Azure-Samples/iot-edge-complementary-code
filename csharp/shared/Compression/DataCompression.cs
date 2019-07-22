403d2cda419c0e287fa38c24cb7299c701c694fe
Alan Pulliam <apulliam@microsoft.com>
2019-07-22 10:43:59 -0700

    simplify C# Compression lib

--- csharp/shared/Compression/DataCompression.cs
+++ csharp/shared/Compression/DataCompression.cs
@@ -1,37 +1,35 @@
-using System;
 using System.IO;
 using System.IO.Compression;
 
-namespace DataCompression {
-
-    public class CompressionClass {
-        public static byte[] Compress(byte[] data){
-                if(data.Length == 0) {
-                    return data;
-                }
-
-                using (MemoryStream compressedDataStream = new MemoryStream(data.Length))
-                {
-                    using (GZipStream zip = new GZipStream(compressedDataStream, CompressionMode.Compress, leaveOpen: true))
-                    {
-                        zip.Write(data, 0, data.Length);
-                    }
-                    return compressedDataStream.ToArray();    
-                }
-        }
-
-        public static byte[] Decompress(byte[] data){
-            if(data.Length == 0){
+namespace DataCompression
+{
+    public class CompressionClass
+    {
+        public static byte[] Compress(byte[] data)
+        {
+            if (data.Length == 0)
+            {
                 return data;
             }
 
-            byte[] gzipSize = new byte[4];
-            gzipSize[0] = data[data.Length -4];
-            gzipSize[1] = data[data.Length -3]; 
-            gzipSize[2] = data[data.Length -2];
-            gzipSize[3] = data[data.Length -1];
+            using (MemoryStream compressedDataStream = new MemoryStream())
+            {
+                using (GZipStream zip = new GZipStream(compressedDataStream, CompressionMode.Compress))
+                {
+                    zip.Write(data, 0, data.Length);
+                }
+                return compressedDataStream.ToArray();
+            }
+        }
 
-            using (MemoryStream decompressedDataStream =new MemoryStream(BitConverter.ToInt32(gzipSize,0)))
+        public static byte[] Decompress(byte[] data)
+        {
+            if (data.Length == 0)
+            {
+                return data;
+            }
+
+            using (MemoryStream decompressedDataStream = new MemoryStream())
             {
                 using (MemoryStream dataStream = new MemoryStream(data, writable: false))
                 {
@@ -40,7 +38,7 @@ namespace DataCompression {
                         zip.CopyTo(decompressedDataStream);
                     }
                 }
-            return decompressedDataStream.ToArray();
+                return decompressedDataStream.ToArray();
             }
         }
     }
